87189be1eba9372ce535ff9c5bd0a77f431fda83
SEND-IT <you@example.com>
2020-10-19 11:36:05 +1300

    add google fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+   
+    font-family: 'Fira Sans', sans-serif;
+
+
+  }
+
+  h1, h2, h3 {
+    font-family: 'Ubuntu', sans-serif;
+  }
+`
 
 function App() {
   return (
-  <div>
-    Hello
-  </div>
+
+    <>
+   <h1>PizzaParty</h1>
+    <GlobalStyle/>
+    <div>Helllooooo</div>
+
+    </>
+
   );
 }
 
